c038e6e576a748432c7c707942bb412913e6522e
Jiran <paiveryhappy@gmail.com>
2023-11-30 15:31:20 +0700

    feat: submit condition-logic exercise

--- ex-3.js
+++ ex-3.js
@@ -2,4 +2,8 @@
 // Ternary Operator
 let lightBulbStatus = "Off";
 
-//Start coding here
+lightBulbStatus === "Off"
+  ? console.log("Light bulb is Off.")
+  : lightBulbStatus === "On"
+  ? console.log("Light bulb is On.")
+  : console.log("Please choose the correct input (On/Off)");
